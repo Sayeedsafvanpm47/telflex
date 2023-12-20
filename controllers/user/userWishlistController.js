c1b6f05e8fea8bddbf45e23f43812e2dd308eb66
Sayeed Safvan <sayeedsafvan123@gmail.com>
2023-12-20 15:31:26 +0530

    wishlist bug cleared

--- controllers/user/userWishlistController.js
+++ controllers/user/userWishlistController.js
@@ -99,38 +99,119 @@ module.exports = {
 		    const userId = req.session.userId;
 		    const { id } = req.query;
 		    console.log(id);
-		    const list = await wishlistModel.findOne({ userId: userId });
+		    const list = await cartModel.findOne({ userId: userId });
+		    const wish = await wishlistModel.findOne({userId:userId}).populate({
+			path: "products.product_id",
+			model: "products",
+			select: "images productName size productDiscount"
+		});
+                        let quantity
+		   
+		    for(item of wish.products)
+		    {
+			if(item.proid.toString() == id)
+			{
+
+				quantity = item.quantity
+				
+				break;
+
+			}
+		    }
+
 		    console.log(list);
 		    console.log(list.products);
-		    let singleId;
-		    let quantity
-		    const cart = [];
-	      
-		    for (const product of list.products) {
-		        if (product.proid.toString() === id) {
-			  productFound = true;
-			  singleId = product.proid;
+		    let existingCart
+                          
+		   for(items of list.products)
+		   {
+		       if(items.single_id.toString() == id)
+		       {
+			existingCart = true
+			items.quantity += parseInt(quantity)
+			                               await list.save()
+
+						return res.redirect("/user/showCart");
+
+
 			
-		        }
-		    }
+		       }
+		   }
+		    
+		//     if(existingCart)
+		//     {
+		// 	existingCart.quantity += +quantity
+                    //            await list.save()
+		// 	return res.redirect("/user/showCart");
+		//     }
+                         if(!existingCart){
+		   const noExistingItem = wish.products.find(item => item.proid.toString() == id)
+		   if(noExistingItem)
+		   {
+			const newCartItem = {
+				product_id: noExistingItem.product_id,
+				
+				quantity: noExistingItem.quantity,
+				size: noExistingItem.size,
+				price: noExistingItem.price,
+				mrp: noExistingItem.mrp,
+				stock: noExistingItem.stock,
+				lastStock: noExistingItem.stock,
+				single_id: noExistingItem.proid,
+				
+			      };
+
+			      list.products.push(newCartItem);
+			      await list.save();
+			      return res.redirect("/user/showCart");
+		    
+		   }
+		}
+		    
+
+
+
+
+		    
+
+
+
+
+
+
+		//     let singleId;
+		//     let quantity
+		//     const cart = [];
 	      
-		    if (productFound) {
-		        const cartFound = await cartModel.findOne({ userId: userId });
+		//     for (const product of list.products) {
+		//         if (product.proid.toString() === id) {
+		// 	  productFound = true;
+		// 	  singleId = product.proid;
+			
+		//         }
+		//     }
 	      
-		        let searchForItem = cartFound.products.find(item => item.single_id === singleId);
-		        if (searchForItem) {
-			   searchForItem.quantity += +quantity
-			  return res.redirect("/user/showCart");
-		        } else {
+		//     if (productFound) {
+		//         const cartFound = await cartModel.findOne({ userId: userId });
+	      
+		//         let searchForItem = cartFound.products.find(item => item.single_id === singleId);
+		//         if (searchForItem) {
+		// 	   searchForItem.quantity += +quantity
+		// 	  return res.redirect("/user/showCart");
+		//         } else {
 			  
-			  cartFound.products = cartFound.products.concat(cart);
-			  await cartFound.save();
-			  return res.redirect("/user/showCart");
-		        }
-		    } else {
+		// 	  cartFound.products = cartFound.products.concat(cart);
+		// 	  await cartFound.save();
+		// 	  return res.redirect("/user/showCart");
+		//         }
+		//     } else {
 		      
-		        return res.redirect("/user/showwishlist");
-		    }
+		//         return res.redirect("/user/showwishlist");
+		//     }
+
+
+
+		
 		} catch (error) {
 		    console.log(error);
 		}
