155c267b612406acdb22d94a6bc254dc97029ce0
Sayeed Safvan <sayeedsafvan123@gmail.com>
2023-12-26 12:49:43 +0530

    final commit

--- app.js
+++ app.js
@@ -6,17 +6,14 @@ const adminRoute = require("./routes/admin/admin");
 const rootRoute = require("./routes/root");
 const dbConnect = require("./config/database");
 const { sessionMiddleware, setNoCache } = require("./middlewares/sessionMiddleware");
-const timeout = require("express-timeout-handler");
 const errorMiddleware = require('./middlewares/errorMiddleware')
-
-const flash = require("connect-flash");
 const error = require("./middlewares/errorMiddleware");
 
 app.use(setNoCache);
 
 app.use(sessionMiddleware);
 
-app.use(flash());
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -31,22 +28,13 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/uploads", express.static("uploads"));
-// app.use(
-// 	timeout.handler({
-// 		timeout: 10000,
-// 		onTimeout: function (req, res) {
-// 			req.session.errorOccured = true;
-// 			res.status(404).redirect("/user/error");
-// 		}
-// 	})
-// );
-// app.use((req, res) => {
-// 	req.session.errorOccured = true;
+app.use((req, res) => {
+	req.session.errorOccured = true;
 
-// 	res.status(404).redirect("/user/error");
-// });
+	res.status(404).redirect("/user/error");
+});
 
-// app.use(error);
+app.use(error);
 
 dbConnect().then(() => {
 	app.listen(8080, () => console.log("listening to port 3000"));
