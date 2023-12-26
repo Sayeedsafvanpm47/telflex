cb7bc6d3e6279836edf4268c98b9cdebf412b68b
Sayeed Safvan <sayeedsafvan123@gmail.com>
2023-12-26 16:14:17 +0530

    testing morgan

--- app.js
+++ app.js
@@ -40,6 +40,18 @@ app.use((req, res) => {
 
 app.use(error);
 
+const logger = winston.createLogger({
+	transports: [
+	  new winston.transports.Console()
+	]
+        });
+        
+       
+        app.use((req, res, next) => {
+	logger.info(`${req.method} ${req.url}`);
+	next();
+        });
+
 dbConnect().then(() => {
 	app.listen(8080, () => console.log("listening to port 3000"));
 });
