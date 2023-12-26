33e3f16457ba77fd981bae88fc4a6b43ab68b8c7
Sayeed Safvan <sayeedsafvan123@gmail.com>
2023-12-26 16:22:20 +0530

    reverted back to the commit before

--- app.js
+++ app.js
@@ -40,17 +40,6 @@ app.use((req, res) => {
 
 app.use(error);
 
-const logger = winston.createLogger({
-	transports: [
-	  new winston.transports.Console()
-	]
-        });
-        
-       
-        app.use((req, res, next) => {
-	logger.info(`${req.method} ${req.url}`);
-	next();
-        });
 
 dbConnect().then(() => {
 	app.listen(8080, () => console.log("listening to port 3000"));
